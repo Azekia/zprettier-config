0b28e203bd5d0a74977b20ca18e48b703b35f872
Antonio Begines <antonio@begines.net>
2023-08-16 09:18:02 +0200

    initial config

---
+++ index.js
@@ -0,0 +1,10 @@
+export default {
+    "tabWidth": 4,
+    "useTabs": false,
+    "singleQuote": true,
+    "bracketSpacing": true,
+    "arrowParens": "always",
+    "printWidth": 136,
+    "semi": true,
+    "trailingComma": "none"
+}
